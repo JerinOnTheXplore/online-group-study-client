1e17ad3c3a0289e2f123dd6d7c2e3f8858a05909
JerinXplore <jerinjerin101325@gmail.com>
2025-06-05 21:01:34 +0600

    Handled conditional login and logout

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -12,7 +12,6 @@ const AuthProvider = ({children}) => {
   }
 
   const signIn = (email,password) =>{
-  setLoading(true);
   return signInWithEmailAndPassword(auth,email,password);
 }
 
@@ -21,7 +20,6 @@ const AuthProvider = ({children}) => {
 };
 
   const googleSignIn = ()=>{
-    setLoading(true);
     const provider = new GoogleAuthProvider();
     return signInWithPopup(auth,provider);
 };
